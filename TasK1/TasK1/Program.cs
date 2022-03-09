72a927658abdfb82d4697edbbc4e796c520c8c2f
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-09 12:38:02 +0400

    Task 1 added

---
+++ TasK1/TasK1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TasK1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number % 7 == 0)
+            {
+                Console.WriteLine("7-e bolunur");
+
+            }
+            else
+            {
+                Console.WriteLine(devide(number));
+            }
+
+        }
+        public static int devide(int num)
+        {
+            int small = num % 7;
+            int big = 7 - small;
+            if (small > big)
+            {
+                return num + big;
+            }
+            else
+            {
+                return num - small;
+            }
+        }
+    }
+}
