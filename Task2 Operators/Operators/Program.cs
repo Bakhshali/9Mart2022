31009ec37c84b9b4fb995299868d5df7652c8998
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-09 12:41:09 +0400

    Task 2 added

---
+++ Task2 Operators/Operators/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("enter number 1");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("enter number 2");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("enter any operator");
+            Console.Write("result");
+        }
+
+
+        
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public static int Sub(int num1,int num2)
+            
+        {   
+            return num1 + num2;
+
+        }
+        public static int Minus(int num1,int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Devide(int num1,int num2)
+        {
+            return num1 / num2;
+        }
+        public static int Multi(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+    }
+
